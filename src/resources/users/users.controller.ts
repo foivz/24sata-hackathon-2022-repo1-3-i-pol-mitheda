0b7569f40c27ffe38845470116f796e29fcf8dd7
jfkeci <ja.lip132@gmail.com>
2022-04-02 15:15:51 +0200

    chore: users crud

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -4,15 +4,20 @@ import { prismaClient } from "../../utils/prisma.utils";
 
 export class UsersController {
   public getUsers = async (req: any, res: any) => {
-    return res.status(200).send("mkmk");
-    const users = await prismaClient.users.findMany();
+    try {
+      const users = await prismaClient.users.findMany({});
 
-    return users;
+      if (!users) throw new HttpException(404, "No users found");
+
+      return users;
+    } catch (error: any) {
+      throw new HttpException(500, JSON.stringify(error));
+    }
   };
 
   createUser = async (req: any, res: any): Promise<any | void> => {
     console.log("REQ-BODY", req.body);
-    const { username, email, password } = req.body;
+    const { username, email, password, cognito_id } = req.body;
 
     try {
       const newUser = await prismaClient.users.create({
@@ -20,6 +25,7 @@ export class UsersController {
           username,
           email,
           password,
+          cognito_id,
         },
       });
 
@@ -28,6 +34,62 @@ export class UsersController {
       res.status(201).json(newUser);
     } catch (error) {
       throw new HttpException(500, "Internal server error");
+    }
+  };
+
+  getUserByEmail = async (req: any, res: any) => {
+    try {
+      const { email } = req.body;
+
+      const user = await prismaClient.users.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) throw new HttpException(404, "No user found");
+
+      return user;
+    } catch (error) {
+      throw new HttpException(500, JSON.stringify(error));
+    }
+  };
+
+  getUserById = async (req: any, res: any) => {
+    try {
+      const { id } = req.params;
+
+      const user = await prismaClient.users.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!user) throw new HttpException(404, "No user found");
+
+      return user;
+    } catch (error) {
+      throw new HttpException(500, JSON.stringify(error));
+    }
+  };
+
+  deleteUser = async (req: any, res: any) => {
+    try {
+      const { id } = req.params;
+
+      if (!id) throw new HttpException(400, 'Missing "id" param');
+
+      const user = prismaClient.users.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!user) throw new HttpException(404, "No user found");
+
+      return res.status(204);
+    } catch (error: any) {
+      throw new HttpException(500, JSON.stringify(error));
     }
   };
 }
